503b411936399b2577fbda810d39cc6e26e9b28c
viniciusth <viniciustht@gmail.com>
2024-03-16 11:20:07 -0300

    some autocmds

--- lua/viniciusth/init.lua
+++ lua/viniciusth/init.lua
@@ -2,3 +2,25 @@ require("viniciusth.remap")
 require("viniciusth.set")
 require("viniciusth.packer").compile()
 
+local augroup = vim.api.nvim_create_augroup
+local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = ThePrimeagenGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
