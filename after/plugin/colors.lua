79c1d5092c1ebb098066e01637150a2c7ad78195
Vinicius Hirschle <viniciustht@gmail.com>
2025-02-12 17:30:29 -0300

    uninstall copilot :)

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -13,8 +13,8 @@ end
 function ColorMyPencils(add)
     local color = getColor(add)
     vim.cmd.colorscheme(color)
-    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 vim.keymap.set("n", "<leader>cs", function() ColorMyPencils(true) end)
